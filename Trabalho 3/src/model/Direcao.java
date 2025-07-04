036ceee145ea67b550ed93e26e81ef7e62bdbf2a
thalles-cg <thalles.pity@gmail.com>
2025-07-04 18:13:28 -0300

    fix: Exclusão de parte desnecessária do enum de direções

--- Trabalho 3/src/model/Direcao.java
+++ Trabalho 3/src/model/Direcao.java
@@ -1,27 +1,10 @@
 package model;
 
 public enum Direcao {
-    CIMA(0, -1),
-    BAIXO(0, 1),
-    ESQUERDA(-1, 0),
-    DIREITA(1, 0),
-    CIMA_ESQUERDA(-1, -1),
-    CIMA_DIREITA(1, -1),
-    BAIXO_ESQUERDA(-1, 1),
-    BAIXO_DIREITA(1, 1);
+    CIMA,
+    BAIXO,
+    ESQUERDA,
+    DIREITA;
 
-    private final int dx, dy;
-
-    Direcao(int dx, int dy) {
-        this.dx = dx;
-        this.dy = dy;
-    }
-
-    public int dx() { return dx; }
-    public int dy() { return dy; }
-
-    public static Direcao fromValor(int val) {
-        return values()[val % values().length];
-    }
 }
 
